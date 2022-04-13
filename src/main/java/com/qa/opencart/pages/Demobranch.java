51b92059b1732277e43e3f0d6bbb90e482320d25
DESKTOP-VC5SNKN\raaga <ragannagarichaitanya1@gmail.com>
2022-04-13 11:55:34 +0530

    modified

---
+++ src/main/java/com/qa/opencart/pages/Demobranch.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class Demobranch {
+
+	public static void main(String[] args) {
+System.out.println("github");
+int i=10;
+System.out.println(i);
+
+	}
+
+}
